d38cae5f6ad50b20e57a26daa6a5d92c24ef50a3
Admin <Admin@supriyavijay>
2024-08-29 11:35:29 +0530

    push

---
+++ DemoJenkins/src/test/java/contactTest/ToCreatecontact.java
@@ -0,0 +1,12 @@
+package contactTest;
+
+public class ToCreatecontact {
+
+	public static void main(String[] args) {
+		System.out.println("Contact Added");
+		System.out.println("contact created successfully");
+		
+
+	}
+
+}
---
+++ DemoJenkins/src/test/java/organizationTest/TocreateOrganization.java
@@ -0,0 +1,11 @@
+package organizationTest;
+
+public class TocreateOrganization {
+
+	public static void main(String[] args) {
+	System.out.println("Organization added");
+	System.out.println("Organization created successfully");
+
+	}
+
+}
